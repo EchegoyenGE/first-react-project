e676fc9cf047d699464b9993b4f878bdc37d0f7d
EchegoyenGE <Gastonec24@gmail.com>
2021-05-02 13:41:11 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,98 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const Title = () => <h1>Search country:</h1>
+
+const List = ({ countries }) => {
+  return (
+    <div className="list">
+        <ul>
+          {
+            countries
+              .map((country) => 
+                <li>
+                  {country.name}
+                </li>
+              )
+          }
+        </ul>
+    </div>
+  )
+}
+
+const Data = ({country}) => {
+
+  console.log(country);
+  return(
+    <div className="data-list">
+      <h1>{country.name}</h1>
+      <h4>Capital: {country.capital}</h4>
+      <h4>Population: {country.population}</h4>
+      <div> <h4>Languages:</h4> 
+        <ul> 
+          { country.languages.map((lang) => <li> {lang.nativeName} </li> ) } 
+        </ul> 
+      </div>
+      <img className="flag" src={country.flag} alt={country.name}/>
+    </div>
+  )
+}
+
+const App = () => {
+  const [countries, setcountries] = useState({
+    originalCountries: [],
+    filtCountries: []
+  });
+  const [newCountry, setnewCountry] = useState("");
+
+  useEffect(() =>{
+
+    setTimeout(() => {
+      fetch("https://restcountries.eu/rest/v2/all")
+        .then(response => response.json())
+        .then((json) => {
+          setcountries({
+            originalCountries: json,
+            filtCountries: json
+          })
+        });
+    }, 1000);
+
+  } ,[]);
+
+  const handleChange = (event) =>{
+
+    const newCount = event.target.value.toLowerCase();
+    setnewCountry(newCount);
+
+    const result = countries.originalCountries.filter((country) => country.name.toLowerCase().includes(newCount));
+    setcountries({
+      ...countries,
+      filtCountries: result
+    });
+  };
+
+  return (
+    <div className="container">
+      <Title />
+      <form>
+        <input type='text'onChange={handleChange}/>
+      </form>
+      { 
+        (countries.filtCountries.length > 1)?
+        <List countries={countries.filtCountries} />:
+        []
+      }
+      {
+        (countries.filtCountries.length === 1)?
+        <Data country={countries.filtCountries[0]}/>:
+        []
+      }
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, 
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
